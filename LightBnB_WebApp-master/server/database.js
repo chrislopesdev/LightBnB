8242104c137c22e055391fdb976bcab72a5e367d
Chris Lopes <chris.lopes@gmail.com>
2022-01-06 21:58:27 +0000

    Update functionality for getting user's reservations.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,8 +83,19 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = (guest_id, limit = 10) => {
+  // eslint-disable-next-line quotes
+  const queryString = `
+    SELECT *
+    FROM properties
+    JOIN reservations ON property_id = properties.id
+    WHERE guest_id = $1
+    LIMIT $2`;
+  const value = [guest_id, limit];
+  return pool
+    .query(queryString, value)
+    .then((result) => result.rows)
+    .catch((err) => console.log('Error: ', err));
 };
 exports.getAllReservations = getAllReservations;
 
